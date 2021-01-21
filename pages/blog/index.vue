576ceb027b3dfecc5552561bb86dfa302b9e73a6
olivervorasai <olivervorasai@gmail.com>
2021-01-20 21:22:46 -0800

    Refactor: Posts are subset of blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,22 +4,22 @@
 
     <v-col cols="12" sm="8" md="6">
       <v-card
-        v-for="blog in blogs"
-        :key="blog.slug"
-        :to="blog.path"
+        v-for="post in blog"
+        :key="post.slug"
+        :to="post.path"
         nuxt
         flat
         class="my-12 highlight"
       >
         <v-row no-gutters>
           <v-col>
-            <v-img max-height="256" max-width="512" :src="blog.image"></v-img>
+            <v-img max-height="256" max-width="512" :src="post.image"></v-img>
           </v-col>
           <v-col>
             <v-card-text>
-              <div>{{ formateDate(blog.updatedAt) }}</div>
-              <p class="display-1 text--primary">{{ blog.title }}</p>
-              <div class="text--primary">{{ blog.description }}</div>
+              <div>{{ formateDate(post.updatedAt) }}</div>
+              <p class="display-1 text--primary">{{ post.title }}</p>
+              <div class="text--primary">{{ post.description }}</div>
             </v-card-text>
           </v-col>
         </v-row>
@@ -31,10 +31,10 @@
 <script>
 export default {
   async asyncData({ $content }) {
-    const blogs = await $content('blogs').fetch()
+    const blog = await $content('blog').fetch()
 
     return {
-      blogs,
+      blog,
     }
   },
   methods: {
@@ -54,7 +54,7 @@ export default {
 <style scoped>
 .highlight {
   border: 1px solid transparent;
-  transition: border 1s;
+  transition: border 0.75s;
 }
 .highlight:hover {
   border: 1px solid red;
