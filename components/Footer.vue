41e6fe702750a0cc36f2ca953adec3e5979111e7
olivervorasai <olivervorasai@gmail.com>
2021-01-22 16:03:54 -0800

    Add navgiation links to footer, remove placeholder text

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,25 +6,25 @@
         <v-row class="footer-text-container my-6" no-gutters>
           <v-col cols="12" md="4">
             <div>
-              <h4>Title</h4>
-              <p>Something</p>
-              <p>Something</p>
+              <h4 class="pb-4">Navigation</h4>
+              <ul>
+                <li>
+                  <nuxt-link to="/about">About Us</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/location">Location</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/blog">Blog</nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/contact">Contact</nuxt-link>
+                </li>
+              </ul>
             </div>
           </v-col>
-          <v-col cols="12" md="4">
-            <div>
-              <h4>Title</h4>
-              <p>Something</p>
-              <p>Something</p>
-            </div>
-          </v-col>
-          <v-col cols="12" md="4">
-            <div>
-              <h4>Title</h4>
-              <p>Something</p>
-              <p>Something</p>
-            </div>
-          </v-col>
+          <v-col cols="12" md="4"> </v-col>
+          <v-col cols="12" md="4"> </v-col>
         </v-row>
         <v-divider></v-divider>
         <v-row class="footer-text-container my-6" justify="end" no-gutters>
@@ -35,7 +35,7 @@
   </v-container>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
   position: relative;
   background-image: linear-gradient(#292e49, #536976);
@@ -49,5 +49,18 @@
 }
 .v-divider {
   border-color: rgba(0, 0, 0, 0.42);
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+
+  li {
+    padding-bottom: 0.5em;
+  }
+  a {
+    text-decoration: none;
+    font-size: 0.875em;
+    color: white;
+  }
 }
 </style>
