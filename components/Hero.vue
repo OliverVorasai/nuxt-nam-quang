62706b725b22fac590d2b937b4c0a80f3eed3d1f
olivervorasai <olivervorasai@gmail.com>
2021-01-27 21:57:43 -0800

    [iss7] Add responsize height and font-size to hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="hero-image grey lighten-2">
-    <v-img :src="randomImage.image" min-height="400" eager>
+    <v-img :src="randomImage.image" height="50vh" eager>
       <v-row class="hero-text">
         <v-col
           v-intersect.once="fadeOnIntersect"
           align-self="center"
           class="fade fade-out"
         >
-          <h2 class="text-center text-h1 font-weight-medium white--text">
+          <h2
+            class="text-center text-h3 text-sm-h2 text-md-h1 font-weight-medium white--text"
+          >
             {{ heroText }}
           </h2>
         </v-col>
