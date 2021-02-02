bc69d3373e6c65aa9118b2e830847395a13042c8
olivervorasai <olivervorasai@gmail.com>
2021-02-01 19:16:07 -0800

    Give image explicit height and width

--- components/LanguageInput.vue
+++ components/LanguageInput.vue
@@ -7,12 +7,16 @@
           :src="require('@/assets/svg/united-states.svg')"
           class="flag"
           aria-hidden="true"
+          height="1.5em"
+          width="1.5em"
         />
         <img
           v-else-if="$i18n.locale === 'vi'"
           :src="require('@/assets/svg/vietnam.svg')"
           class="flag"
           aria-hidden="true"
+          height="1.5em"
+          width="1.5em"
         />
         {{ getLanguage }}
       </v-btn>
@@ -48,6 +52,7 @@ export default {
 <style scoped>
 .flag {
   height: 1.5em;
+  width: 1.5em;
   margin-right: 0.5em;
 }
 </style>
